01c937aaf627a1275e468c8cd39944998c692058
Marcus <marc303i@stud.kea.dk>
2025-03-21 11:22:51 +0100

    started on the docker stuff

--- admin_microservice/main.py
+++ admin_microservice/main.py
@@ -16,15 +16,11 @@ app.add_middleware(CORSMiddleware, **CORS_SETTINGS)
 
 @app.get("/")
 def read_root():
-    return {"Hello": "Admin Service"}
+    return {"Hello": "Admin Service something EXTRA"}
 
 @app.get("/send_message/{message}")
 def send_message(message: str):
     send_trial_message(message)
     return {"message": "Message sent successfully"}
 
-# To run the admin microservice endpoints, run this script while in the root of the project directory:
-# poetry run python -m admin_microservice.main
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+
--- admin_microservice/rabbitmq/publisher.py
+++
@@ -1,6 +0,0 @@
-from modules import FanoutPublisher
-
-
-class TrialPublisher(FanoutPublisher):
-    def __init__(self):
-        super().__init__('trial_admin_exchange')
