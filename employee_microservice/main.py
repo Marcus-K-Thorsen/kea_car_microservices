6c037701e928a66a4f4b950dcbc27dc4891a3fb7
OliverRoat <oliver.roat@gmail.com>
2025-04-04 12:27:19 +0200

    added setup for employee and sync folders

--- employee_microservice/main.py
+++ employee_microservice/main.py
@@ -1,44 +1,43 @@
 # External Library imports
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI
-from pydantic import BaseModel
-from .rabbitmq.publisher import MessagePublisher as Publisher
+# from fastapi.middleware.cors import CORSMiddleware
+# from fastapi import FastAPI
+# from pydantic import BaseModel
 
-class TrialItem(BaseModel):
-    item_name: str
+# class TrialItem(BaseModel):
+#     item_name: str
 
-class Trial(BaseModel):
-    name: str
-    age: int
-    trial_item: TrialItem
+# class Trial(BaseModel):
+#     name: str
+#     age: int
+#     trial_item: TrialItem
 
-app = FastAPI()
+# app = FastAPI()
 
-CORS_SETTINGS = {
-    "allow_origins": ["*"],
-    "allow_credentials": True,
-    "allow_methods": ["*"],
-    "allow_headers": ["*"]
-}
+# CORS_SETTINGS = {
+#     "allow_origins": ["*"],
+#     "allow_credentials": True,
+#     "allow_methods": ["*"],
+#     "allow_headers": ["*"]
+# }
 
-app.add_middleware(CORSMiddleware, **CORS_SETTINGS)
+# app.add_middleware(CORSMiddleware, **CORS_SETTINGS)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "Employee Service"}
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "Employee Service"}
 
-@app.get("/send_message/{message}")
-def send_message(message: str):
-    trial_item = TrialItem(item_name=message)
-    trial = Trial(name="employee", age=20, trial_item=trial_item)
+# @app.get("/send_message/{message}")
+# def send_message(message: str):
+#     trial_item = TrialItem(item_name=message)
+#     trial = Trial(name="employee", age=20, trial_item=trial_item)
     
-    message_publisher = Publisher()
-    message_publisher.publish(trial)
-    message_publisher.close_connection()
-    return {"message": "Message sent successfully"}
+#     message_publisher = Publisher()
+#     message_publisher.publish(trial)
+#     message_publisher.close_connection()
+#     return {"message": "Message sent successfully"}
 
-# To run the employee microservice endpoints, run this script while in the root of the project directory:
-# poetry run python -m employee_microservice.main
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8003)
+# # To run the employee microservice endpoints, run this script while in the root of the project directory:
+# # poetry run python -m employee_microservice.main
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="127.0.0.1", port=8003)
--- employee_microservice/rabbitmq/publisher.py
+++
@@ -1,5 +0,0 @@
-from modules import FanoutPublisher
-
-class MessagePublisher(FanoutPublisher):
-    def __init__(self):
-        super().__init__('trial_employee_exchange')
