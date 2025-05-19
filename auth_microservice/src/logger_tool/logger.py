6e993111d1b8cdce12dd2e98cc209e33660bf4d0
OliverRoat <oliver.roat@gmail.com>
2025-05-19 10:17:34 +0200

    logger DOING

--- auth_microservice/src/logger_tool/logger.py
+++ auth_microservice/src/logger_tool/logger.py
@@ -5,3 +5,9 @@ logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
 logger = logging.getLogger(__name__)
+
+# Add file handler to log to a file as well
+file_handler = logging.FileHandler("auth_microservice.log")
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+logger.addHandler(file_handler)
