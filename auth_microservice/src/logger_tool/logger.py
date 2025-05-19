e9cd6853d6567a5de8b877303a053a916975675c
OliverRoat <oliver.roat@gmail.com>
2025-05-19 11:24:04 +0200

    added logging path to logger

--- auth_microservice/src/logger_tool/logger.py
+++ auth_microservice/src/logger_tool/logger.py
@@ -1,4 +1,9 @@
+import os
 import logging
+
+# Ensure log directory exists
+log_dir = "var/log/auth_microservice"
+os.makedirs(log_dir, exist_ok=True)
 
 # Configure logging
 logging.basicConfig(
@@ -7,7 +12,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # Add file handler to log to a file as well
-file_handler = logging.FileHandler("var/log/auth_microservice/auth_microservice.log")
+file_handler = logging.FileHandler(os.path.join(log_dir, "auth_microservice.log"))
 file_handler.setLevel(logging.INFO)
 file_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
 logger.addHandler(file_handler)
